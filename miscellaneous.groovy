d17a850a41b9a7027a3225a1e0d917fdcd516f89
Albert King <thealbertking@gmail.com>
2025-12-08 11:13:33 -0500

    add scratch paper script to troubleshoot in

--- miscellaneous.groovy
+++ miscellaneous.groovy
@@ -4,3 +4,15 @@ print "Hello, World!"
 /* so println adds a \n at the end of the print - skips to a new line
 whereas print does not. */
 
+
+// Make a SQL connection, run query:
+/* Full example code from jdbc website:
+Statement st = conn.createStatement();
+ResultSet rs = st.executeQuery("SELECT * FROM mytable WHERE columnfoo = 500");
+while (rs.next()) {
+    System.out.print("Column 1 returned ");
+    System.out.println(rs.getString(1));
+}
+rs.close();
+st.close();
+*/
